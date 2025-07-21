630bdf3b778ebe5cebb86e72be91c3c8c34cc999
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-07-22 06:27:52 +0700

    Implement authentication routes, console commands, and profile management features
    - Added authentication routes for registration, login, password reset, and email verification in `routes/auth.php`.
    - Created console command to display an inspiring quote in `routes/console.php`.
    - Defined web routes for dashboard and profile management in `routes/web.php`.
    - Updated `.gitignore` files in various storage directories to exclude all files except `.gitignore`.
    - Added Tailwind CSS configuration in `tailwind.config.js`.
    - Implemented feature tests for authentication, email verification, password confirmation, password reset, and profile management in the `tests/Feature/Auth` directory.
    - Created base test case class in `tests/TestCase.php` and example tests in `tests/Unit/ExampleTest.php`.
    - Configured Vite for asset management in `vite.config.js`.

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'judul',
+        'deskripsi',
+        'tanggal_event',
+    ];
+
+    public function users():BelongsTo {
+        return $this->belongsTo(User::class); 
+    }
+}
