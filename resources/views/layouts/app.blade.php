92060b4af3314d52a66890e9e589aa9b9f896891
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-07-22 07:46:30 +0700

    Add DashboardUserController and update dashboard view with user greeting and add event button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <!-- Icons -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-pT1CwYOtpnP9OcToEoH9n2Sn5...etc" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
