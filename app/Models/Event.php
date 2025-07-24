25dd5cb87f73ace6ebe15f2d1a09fe5021ba41ea
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-07-24 16:27:43 +0700

    add: menambahkan accessor untuk menandai countdown waktu event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -21,8 +21,28 @@ class Event extends Model
     }
 
     public function getTanggalFormattedAttribute(){
-        Carbon::setLocale('id');
-
         return Carbon::parse($this->tanggal_event)->translatedFormat('l, d F Y, H:i');
     }
+
+    public function getWaktuEventAttribute()
+    {
+        $now = now();
+        $eventTime = Carbon::parse($this->tanggal_event)->timezone(config('app.timezone'));
+        $selisihJam = $now->diffInHours($eventTime, false);
+
+        if ($now->lessThan($eventTime) && $selisihJam < 24) {
+            return 'Event dimulai dalam ' . $eventTime->diffForHumans($now, true) . ' lagi';
+        }
+
+        if ($now->greaterThan($eventTime) && abs($selisihJam) <= 12) {
+            return 'Event sedang berlangsung sejak ' . $eventTime->diffForHumans($now);
+        }
+
+        if ($now->greaterThan($eventTime) && abs($selisihJam) > 12) {
+            return 'Event telah berakhir';
+        }
+
+        return null;
+    }
+
 }
