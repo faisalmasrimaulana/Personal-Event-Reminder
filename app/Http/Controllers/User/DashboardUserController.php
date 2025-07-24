90943eb42bf4359da6799c3ea8c096b44a0ce020
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-07-24 16:26:54 +0700

    delete: menghapus fungsi yang tidak berguna dari resource

--- app/Http/Controllers/User/DashboardUserController.php
+++ app/Http/Controllers/User/DashboardUserController.php
@@ -15,55 +15,7 @@ class DashboardUserController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $events = Event::where('user_id', $user->id)->latest()->paginate(10);
+        $events = Event::where('user_id', $user->id)->orderBy('tanggal_event', 'asc')->paginate(10);
         return view('dashboard', compact('user', 'events'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show()
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
